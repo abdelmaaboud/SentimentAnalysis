be296f94032b2c8e4c8f750b2580cf5ca00cc258
abdelmaaboud <islamabdelmaaboud@yahoo.com>
2017-02-07 16:33:59 +0200

    change in functions and rename nokia file

--- ReviewsAnalyser.py
+++ ReviewsAnalyser.py
@@ -7,19 +7,26 @@ class Analyser:
     def __init__(self, reviews_list,aspects_list):
         self.reviews = reviews_list
         self.aspects=aspects_list
-        self.chuncked = []
-    def chunk_reviews(self):
+        self.chunked_noun_phrases = []
+        self.aspect_dictionary = dict()
+    def get_noun_phrases(self):
         for review in self.reviews:
             token = nltk.word_tokenize(review)
             tagged = nltk.pos_tag(token)
             chunkparser = nltk.RegexpParser(self.chunkgram)
-            self.chuncked.append(chunkparser.parse(tagged))
-        return self.chuncked
+            self.chunked_noun_phrases.append(chunkparser.parse(tagged))
+    def evaluate_polarity(self):
+        # we will implement this function
+        # use noun phrases as input  and return aspect dictionary
+        print("dummy")
+
+
     def analyse_reviews(self):
         # call all functions to analyse reviews
         # return the output
-        chunked = self.chunk_reviews()
+        self.get_noun_phrases()
+        self.evaluate_polarity()
         # we will add functions when complete algorithm implementation
-        return chunked
+        return self.aspect_dictionary
 
 
