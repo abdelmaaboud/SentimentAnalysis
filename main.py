204810213780d2abf4e03692e2ec1c02fa44564d
abdelmaaboud <islamabdelmaaboud@yahoo.com>
2017-02-22 21:00:28 +0200

    change in main file

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-from  ReviewsAnalyser import Analyser
+from ReviewsAnalyser import Analyser
+from AspectExtractor import Aspect
+
+import matplotlib.pyplot as plt
+def draw_graph_(dict):
+    aspects_names = []
+    pos= []
+    neg= []
+    for item in dict:
+        if dict[item]["pos"] == 0 and dict[item]["neg"]== 0:
+            continue
+
+        aspects_names.append(item)
+        pos.append(dict[item]["pos"])
+        neg.append(dict[item]["neg"])
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    # data to plot
+    n_groups = len(aspects_names)
+
+    # create plot
+    fig, ax = plt.subplots()
+    index = np.arange(n_groups)
+    bar_width = 0.35
+    opacity = 0.8
+
+    rects1 = plt.bar(index, pos, bar_width,
+                     alpha=opacity,
+                     color='b',
+                     label='pos')
+
+    rects2 = plt.bar(index + bar_width, neg, bar_width,
+                     alpha=opacity,
+                     color='g',
+                     label='neg')
+
+    plt.xlabel('aspects')
+    plt.ylabel('polarity')
+    plt.title('polarity of aspects')
+    plt.xticks(index + bar_width, aspects_names)
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+
+
 all_reviews = []
 all_aspects=[]
-file = open("Nokia 6610.txt")
+file_name = "Nokia 6610.txt"
+file = open(file_name)
 for line in file :
     if "##" in line:
         sentences = line.split("##")
         for i in range(1,len(sentences)):
             all_reviews.append(sentences[i])
 
-file2 = open("Aspects.txt")
-aspects = file2.read()
-analyser  = Analyser(reviews_list=all_reviews,aspects_list=all_aspects)
+
+aspect = Aspect(all_reviews)
+aspect_list_dictinary = aspect.extract_aspects()
+aspect_list = list(aspect_list_dictinary.keys())
+analyser = Analyser(all_reviews,aspect_list,aspect_list_dictinary)
 output = analyser.analyse_reviews()
-for i in output:
-    print(i)
+print(output)
+draw_graph_(output)
